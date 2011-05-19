6c654bedfff73c2878a01007e451cc72723bf0bd
stephbu <stephbu@microsoft.com>
2011-05-18 23:06:15 -0700

    Cleaned up code and added a solution to project 25 using BigInteger.  Added BigInteger support to the fibonacci sequence generator.

--- ProjectEuler/Fibonnaci.cs
+++ ProjectEuler/Fibonnaci.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace ProjectEuler
 {
-    class Fibonnaci
+    public class Fibonnaci
     {
         public static IEnumerable<int> Sequence
         {
@@ -16,6 +14,26 @@ namespace ProjectEuler
                 int n2 = 0;
                 int n1 = 1;
                 int nextNumber;
+                while (true)
+                {
+                    nextNumber = n1 + n2;
+                    yield return nextNumber;
+                    n2 = n1;
+                    n1 = nextNumber;
+                }
+
+            }
+        }
+
+        public static IEnumerable<BigInteger> BigIntegerSequence
+        {
+            get
+            {
+                yield return 0;
+                yield return 1;
+                BigInteger n2 = 0;
+                BigInteger n1 = 1;
+                BigInteger nextNumber;
                 while (true)
                 {
                     nextNumber = n1 + n2;
---
+++ ProjectEuler/Problem25.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public class Problem25
+    {
+        public static void GetThousandDigitFibonacci()
+        {
+            BigInteger thousandDigit = Fibonnaci.BigIntegerSequence.SkipWhile(num => num.ToString().Length < 1000).First();
+            System.Diagnostics.Debug.WriteLine(thousandDigit);
+        }
+    }
+}
