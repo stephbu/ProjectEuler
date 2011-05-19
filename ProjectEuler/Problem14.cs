71578a2b534c1fba4252d2568a511a89165812d5
stephbu <stephbu@microsoft.com>
2011-05-19 08:34:16 -0700

    Problem 14

---
+++ ProjectEuler/Problem14.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem14
+    {
+        public static void LargestChain()
+        {
+            int longestLength = 0;
+            long longest = 0;
+            foreach (long seed in Enumerable.Range(1, 999999))
+            {
+                int length = Sequence(seed).Count();
+                if (length > longestLength)
+                {
+                    longestLength = length;
+                    longest = seed;
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(longest);
+        }
+
+        public static IEnumerable<long> Sequence (long seed)
+        {
+            yield return seed;
+            while (seed > 1)
+            {
+                if (seed % 2 == 0)
+                {
+                    seed = seed / 2;
+                }
+                else
+                {
+                    seed = (3 * seed) + 1; 
+                }
+                if (seed < 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                yield return seed;
+            }
+        }
+
+    }
+}
