4772c2dc3c80dba5e73debc7ac16517f88274eca
stephbu <stephbu@microsoft.com>
2011-05-19 07:53:34 -0700

    Problem 7 solved...

---
+++ ProjectEuler/Problem7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem7
+    {
+        public static void Prime10001()
+        {
+            long prime10001 = Prime.Sequence.Take(10001).Last();
+            System.Diagnostics.Debug.WriteLine(prime10001);
+        }
+    }
+}
